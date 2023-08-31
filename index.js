25088e49d32d9b11a22075e812ade7ccd412578e
Aguspala1 <aguspalavecino3568@gmail.com>
2023-08-31 19:09:41 -0300

    primera subida

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let Nombre="agus";
+let Apellido="pala";
+let NombreDeUsuario ="ag"
+let Edad="22";
+let CorreoElectronico="aguspalavecino3568@gmail.com";
+let MayorDeEdad=true;
+let Sueldo=300;
+let Gastos=100;
+
+alert (`Nombre Completo= ${Nombre + " " + Apellido}`);
+alert("Dinero Disponible=" +(Sueldo - Gastos));
